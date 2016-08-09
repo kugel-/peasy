kugel-/peasy

commit c31cf153786d2e0609710befaf3b48ee6cfd0deb

lib: solution for API, ABI and other requirements
- Plugins now *must* specify the X-Peasy-API key in their .plugin file. The
  value  must match Geany (it's passed through geany_plugin_register).
- Native plugins must, in addition, call peasy_check_abi() in their
  peas_register_types function (which libpeas hooks up anyway). Here
  peasy records the ABI version which is also passed through
  geany_plugin_register().
- Additionally, plugins can optionally specify glib and gtk versions
  via the X-Peasy-Requires key. Here, a sequence of tests with pkg-config names
  can be specified, e.g. "gtk2 >= 2.24 | gtk3 >= 3.10, glib2 >= 2.32"

lib/peasyobject.c
@@ -29,6 +29,7 @@ typedef struct _PeasyObjectClass PeasyObjectClass;
 typedef struct _PeasySignals PeasySignals;
 typedef struct _PeasySignalsClass PeasySignalsClass;
 #define _g_object_unref0(var) ((var == NULL) ? NULL : (var = (g_object_unref (var), NULL)))
+#define _g_hash_table_unref0(var) ((var == NULL) ? NULL : (var = (g_hash_table_unref (var), NULL)))
 typedef struct _PeasyObjectPrivate PeasyObjectPrivate;
 typedef struct _PeasySignalsPrivate PeasySignalsPrivate;
 
@@ -79,6 +80,7 @@ extern GeanyPlugin* peasy_peasy_plugin;
 GeanyPlugin* peasy_peasy_plugin = NULL;
 extern PeasySignals* peasy_peasy_signals;
 PeasySignals* peasy_peasy_signals = NULL;
+extern GHashTable* peasy_native_abis;
 static gpointer peasy_object_parent_class = NULL;
 static gpointer peasy_signals_parent_class = NULL;
 
@@ -88,6 +90,7 @@ GType peasy_signals_get_type (void) G_GNUC_CONST;
 void peasy_static_init (GeanyPlugin* p);
 PeasySignals* peasy_signals_new (void);
 PeasySignals* peasy_signals_construct (GType object_type);
+static void _g_free0_ (gpointer var);
 #define PEASY_OBJECT_GET_PRIVATE(o) (G_TYPE_INSTANCE_GET_PRIVATE ((o), PEASY_TYPE_OBJECT, PeasyObjectPrivate))
 enum  {
 	PEASY_OBJECT_DUMMY_PROPERTY,
@@ -122,19 +125,32 @@ PeasyFiletype* peasy_filetype_construct (GType object_type, GeanyFiletype* ft);
 static void g_cclosure_user_marshal_VOID__OBJECT_OBJECT (GClosure * closure, GValue * return_value, guint n_param_values, const GValue * param_values, gpointer invocation_hint, gpointer marshal_data);
 
 
+static void _g_free0_ (gpointer var) {
+	var = (g_free (var), NULL);
+}
+
+
 void peasy_static_init (GeanyPlugin* p) {
 	GeanyPlugin* _tmp0_ = NULL;
+	GeanyPlugin* _tmp5_ = NULL;
 	g_return_if_fail (p != NULL);
 	_tmp0_ = peasy_peasy_plugin;
 	if (_tmp0_ == NULL) {
-		GeanyPlugin* _tmp1_ = NULL;
-		PeasySignals* _tmp2_ = NULL;
-		_tmp1_ = p;
-		peasy_peasy_plugin = _tmp1_;
-		_tmp2_ = peasy_signals_new ();
+		PeasySignals* _tmp1_ = NULL;
+		GHashFunc _tmp2_ = NULL;
+		GEqualFunc _tmp3_ = NULL;
+		GHashTable* _tmp4_ = NULL;
+		_tmp1_ = peasy_signals_new ();
 		_g_object_unref0 (peasy_peasy_signals);
-		peasy_peasy_signals = _tmp2_;
+		peasy_peasy_signals = _tmp1_;
+		_tmp2_ = g_str_hash;
+		_tmp3_ = g_str_equal;
+		_tmp4_ = g_hash_table_new_full (_tmp2_, _tmp3_, _g_free0_, NULL);
+		_g_hash_table_unref0 (peasy_native_abis);
+		peasy_native_abis = _tmp4_;
 	}
+	_tmp5_ = p;
+	peasy_peasy_plugin = _tmp5_;
 }
 
 

lib/peasyobject.vala
@@ -13,9 +13,10 @@ public void static_init(Geany.Plugin p)
 {
     if (peasy_plugin == null)
     {
-        peasy_plugin = p;
         peasy_signals = new Signals();
+        Peasy.native_abis = new GLib.HashTable<string, int>(GLib.str_hash, GLib.str_equal);
     }
+    peasy_plugin = p;
 }
 
 public class Object : GLib.Object

lib/Peasy-0.1.gir
@@ -6,6 +6,7 @@
 <include name="GeanyScintilla" version="1.0"/>
 <include name="Gtk" version="3.0"/>
 <include name="Gdk" version="3.0"/>
+<include name="Peas" version="1.0"/>
 <package name="libpeasy"/>
 <c:include name="peasy.h"/>
 <namespace name="Peasy" version="0.1" shared-library="libpeasy.so.0" c:prefix="Peasy">
@@ -790,6 +791,12 @@
 			<type name="GObject.TypeInterface" c:type="GTypeInterface"/>
 		</field>
 	</record>
+	<field name="peasy_native_abis">
+		<type name="GLib.HashTable" c:type="GHashTable*">
+			<type name="utf8" c:type="gchar*"/>
+			<type name="gint" c:type="gint"/>
+		</type>
+	</field>
 	<field name="peasy_peasy_plugin">
 		<type name="Geany.Plugin" c:type="GeanyPlugin*"/>
 	</field>
@@ -808,6 +815,19 @@
 		<parameters>
 			<parameter name="msgid" transfer-ownership="none">
 				<type name="utf8" c:type="const gchar*"/>
+			</parameter>
+		</parameters>
+	</function>
+	<function name="check_abi" c:identifier="peasy_check_abi">
+		<return-value transfer-ownership="full">
+			<type name="gboolean" c:type="gboolean"/>
+		</return-value>
+		<parameters>
+			<parameter name="mod" transfer-ownership="none">
+				<type name="Peas.ObjectModule" c:type="PeasObjectModule*"/>
+			</parameter>
+			<parameter name="abi_ver" transfer-ownership="none">
+				<type name="gint" c:type="gint"/>
 			</parameter>
 		</parameters>
 	</function>

lib/peasy.h
@@ -7,6 +7,7 @@
 #include <glib.h>
 #include <stdlib.h>
 #include <string.h>
+#include <libpeas/peas.h>
 #include <gtk/gtk.h>
 #include <glib-object.h>
 #include <geanyplugin.h>
@@ -236,6 +237,8 @@ struct _PeasyKeyGroupClass {
 
 const gchar* peasy_get_locale_dir (void);
 const gchar* peasy_gettext (const gchar* msgid);
+extern GHashTable* peasy_native_abis;
+gboolean peasy_check_abi (PeasObjectModule* mod, gint abi_ver);
 GType peasy_plugin_configure_get_type (void) G_GNUC_CONST;
 GtkWidget* peasy_plugin_configure_configure (PeasyPluginConfigure* self, GtkDialog* parent);
 GType peasy_plugin_help_get_type (void) G_GNUC_CONST;

lib/plugin.c
@@ -6,9 +6,11 @@
 #include <glib-object.h>
 #include <stdlib.h>
 #include <string.h>
+#include <libpeas/peas.h>
 #include <gtk/gtk.h>
 #include <geanyplugin.h>
 
+#define _g_free0(var) (var = (g_free (var), NULL))
 
 #define PEASY_TYPE_PLUGIN_CONFIGURE (peasy_plugin_configure_get_type ())
 #define PEASY_PLUGIN_CONFIGURE(obj) (G_TYPE_CHECK_INSTANCE_CAST ((obj), PEASY_TYPE_PLUGIN_CONFIGURE, PeasyPluginConfigure))
@@ -102,11 +104,15 @@ struct _PeasyPluginClass {
 };
 
 
+extern GHashTable* peasy_native_abis;
+GHashTable* peasy_native_abis = NULL;
 static gpointer peasy_plugin_parent_class = NULL;
 static PeasyPluginIfaceIface* peasy_plugin_peasy_plugin_iface_parent_iface = NULL;
 
 const gchar* peasy_get_locale_dir (void);
 const gchar* peasy_gettext (const gchar* msgid);
+gboolean peasy_do_check_abi (gint abi);
+gboolean peasy_check_abi (PeasObjectModule* mod, gint abi_ver);
 GType peasy_plugin_configure_get_type (void) G_GNUC_CONST;
 GtkWidget* peasy_plugin_configure_configure (PeasyPluginConfigure* self, GtkDialog* parent);
 GType peasy_plugin_help_get_type (void) G_GNUC_CONST;
@@ -129,6 +135,49 @@ PeasyKeyGroup* peasy_plugin_add_key_group (PeasyPlugin* self, const gchar* secti
 PeasyKeyGroup* peasy_key_group_new_from_geany (GeanyKeyGroup* kb_group);
 PeasyKeyGroup* peasy_key_group_construct_from_geany (GType object_type, GeanyKeyGroup* kb_group);
 static void peasy_plugin_finalize (GObject* obj);
+
+
+/**
+ * Function that native plugins must call in their peas_register_types().
+ *
+ * @param mod The ObjectModule passed to peas_register_types() by libpeas
+ * @param abi_ver The ABI version the plugin was compiled against,
+ *                 simple pass GEANY_API_VERSION.
+ *
+ * @return true if the ABI check has passed, false otherwise. Do not
+ *          proceed calling peas_object_module_register_extension_type()
+ *          on false.
+ */
+gboolean peasy_check_abi (PeasObjectModule* mod, gint abi_ver) {
+	gboolean result = FALSE;
+	gint _tmp0_ = 0;
+	gboolean _tmp1_ = FALSE;
+	g_return_val_if_fail (mod != NULL, FALSE);
+	_tmp0_ = abi_ver;
+	_tmp1_ = peasy_do_check_abi (_tmp0_);
+	if (!_tmp1_) {
+		GHashTable* _tmp2_ = NULL;
+		PeasObjectModule* _tmp3_ = NULL;
+		gchar* _tmp4_ = NULL;
+		gchar* _tmp5_ = NULL;
+		gchar* _tmp6_ = NULL;
+		gchar* _tmp7_ = NULL;
+		gint _tmp8_ = 0;
+		_tmp2_ = peasy_native_abis;
+		_tmp3_ = mod;
+		g_object_get (_tmp3_, "module-name", &_tmp4_, NULL);
+		_tmp5_ = _tmp4_;
+		_tmp6_ = _tmp5_;
+		_tmp7_ = g_strdup (_tmp6_);
+		_tmp8_ = abi_ver;
+		g_hash_table_replace (_tmp2_, _tmp7_, (gpointer) ((gintptr) _tmp8_));
+		_g_free0 (_tmp6_);
+		result = FALSE;
+		return result;
+	}
+	result = TRUE;
+	return result;
+}
 
 
 GtkWidget* peasy_plugin_configure_configure (PeasyPluginConfigure* self, GtkDialog* parent) {

lib/plugin.vala
@@ -5,6 +5,34 @@ namespace Peasy
 public extern unowned string get_locale_dir();
 public extern unowned string gettext(string msgid);
 
+/* GEANY_API_VERSION is not in the .vapi :( */
+private extern bool do_check_abi(int abi);
+
+/* instantiated in Peasy.static_init() */
+protected GLib.HashTable<string, int> native_abis;
+
+/**
+ * Function that native plugins must call in their peas_register_types().
+ *
+ * @param mod The ObjectModule passed to peas_register_types() by libpeas
+ * @param abi_ver The ABI version the plugin was compiled against,
+ *                 simple pass GEANY_API_VERSION.
+ *
+ * @return true if the ABI check has passed, false otherwise. Do not
+ *          proceed calling peas_object_module_register_extension_type()
+ *          on false.
+ */
+public bool check_abi(Peas.ObjectModule mod, int abi_ver)
+{
+    if (!do_check_abi(abi_ver))
+    {
+        native_abis.replace(mod.module_name.dup(), abi_ver);
+        return false;
+    }
+
+    return true;
+}
+
 public interface PluginConfigure
 {
     public abstract Gtk.Widget configure(Gtk.Dialog parent);

plugins/console-peasy.plugin.desktop.in
@@ -5,3 +5,4 @@ _Name=Python Console (Peasy)
 _Description=This plugins opens a console window in which you can explore the Geany APIs and run arbitrary commands. Heavily based on console.py for geanypy.
 Authors=Matthew Brush <codebrainz@codebrainz.ca>, Thomas Martitz <kugel@rockbox.org>
 Copyright=Copyright © 2011 Matthew Brush
+X-Peasy-API=228

plugins/modeline.plugin.desktop.in
@@ -5,3 +5,4 @@ _Name=Vim modeline parser
 _Description=Parses vim modeline and applies them to Geany on a per-file basis.
 Authors=Thomas Maritiz <kugel@rockbox.org>
 Copyright=Copyright © 2016 Thomas Martitz
+X-Peasy-API=228

commit fcb4d4e4528d2271af14769c8a7d546e6a9ad76b

lib: fix assertion failure introduced by c31cf153786d2e0609710befaf3b48ee6cfd0deb
Global peasy_plugin pointer was NULL when Peasy.Signals is instantiated. The
result was an assert failure and non-functional Peasy.Signals wrapper.

lib/peasyobject.c
@@ -132,25 +132,25 @@ static void _g_free0_ (gpointer var) {
 
 void peasy_static_init (GeanyPlugin* p) {
 	GeanyPlugin* _tmp0_ = NULL;
-	GeanyPlugin* _tmp5_ = NULL;
 	g_return_if_fail (p != NULL);
 	_tmp0_ = peasy_peasy_plugin;
 	if (_tmp0_ == NULL) {
-		PeasySignals* _tmp1_ = NULL;
-		GHashFunc _tmp2_ = NULL;
-		GEqualFunc _tmp3_ = NULL;
-		GHashTable* _tmp4_ = NULL;
-		_tmp1_ = peasy_signals_new ();
+		GeanyPlugin* _tmp1_ = NULL;
+		PeasySignals* _tmp2_ = NULL;
+		GHashFunc _tmp3_ = NULL;
+		GEqualFunc _tmp4_ = NULL;
+		GHashTable* _tmp5_ = NULL;
+		_tmp1_ = p;
+		peasy_peasy_plugin = _tmp1_;
+		_tmp2_ = peasy_signals_new ();
 		_g_object_unref0 (peasy_peasy_signals);
-		peasy_peasy_signals = _tmp1_;
-		_tmp2_ = g_str_hash;
-		_tmp3_ = g_str_equal;
-		_tmp4_ = g_hash_table_new_full (_tmp2_, _tmp3_, _g_free0_, NULL);
+		peasy_peasy_signals = _tmp2_;
+		_tmp3_ = g_str_hash;
+		_tmp4_ = g_str_equal;
+		_tmp5_ = g_hash_table_new_full (_tmp3_, _tmp4_, _g_free0_, NULL);
 		_g_hash_table_unref0 (peasy_native_abis);
-		peasy_native_abis = _tmp4_;
+		peasy_native_abis = _tmp5_;
 	}
-	_tmp5_ = p;
-	peasy_peasy_plugin = _tmp5_;
 }
 
 

lib/peasyobject.vala
@@ -13,10 +13,10 @@ public void static_init(Geany.Plugin p)
 {
     if (peasy_plugin == null)
     {
+        peasy_plugin = p;
         peasy_signals = new Signals();
         Peasy.native_abis = new GLib.HashTable<string, int>(GLib.str_hash, GLib.str_equal);
     }
-    peasy_plugin = p;
 }
 
 public class Object : GLib.Object