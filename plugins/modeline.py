86726a08e875bc539b1f9dd90c632cfe730c341b
Thomas Martitz <kugel@rockbox.org>
2016-08-12 07:07:34 +0200

    modeline: refactoring
    - unit tests for regexes
    - use a single regex for both modeline styles since they are similar enough
    - simplify parsing code (get rid of exception handling)
    - remove test preference
    - improve i18n

--- plugins/modeline.py
+++ plugins/modeline.py
@@ -11,6 +11,39 @@ from gi.repository import Geany
 from gi.repository import GeanyScintilla
 from gi.repository import Peasy
 
+#The first form:
+#       [text]{white}{vi:|vim:|ex:}[white]{options}
+#
+# [text]          any text or empty
+# {white}         at least one blank character (<Space> or <Tab>)
+# {vi:|vim:|ex:}  the string "vi:", "vim:" or "ex:"
+# [white]         optional white space
+# {options}       a list of option settings, separated with white space or ':',
+#                 where each part between ':' is the argument for a ":set"
+#                 command (can be empty)
+#
+# Example:
+#    vi:noai:sw=3 ts=6 ~
+#~ vim_com1 = re.compile("(?:vi|vim|ex):\s*(.*[a-zA-Z0-9])$")
+
+#      The second form (this is compatible with some versions of Vi):
+#        [text]{white}{vi:|vim:|ex:}[white]se[t] {options}:[text]
+#
+# [text]          any text or empty
+# {white}         at least one blank character (<Space> or <Tab>)
+# {vi:|vim:|ex:}  the string "vi:", "vim:" or "ex:"
+# [white]         optional white space
+# se[t]           the string "set " or "se " (note the space)
+# {options}       a list of options, separated with white space, which is the
+#                 argument for a ":set" command
+# :               a colon
+# [text]          any text or empty
+#
+# Example:
+#    /* vim: set ai tw=75: */ ~
+vim_reg = "(?:vi|vim|ex):\s*(.*)\s*"
+kv_reg  = "\s|:"
+
 class ModelineKeyFile:
     def __init__(self, datapath):
         self.path = os.path.join(datapath, "plugins", "modeline.conf")
@@ -73,55 +106,41 @@ class Pref:
             if ((self.value != self.default) or (self.key in keyfile.get_keys(self.group)[0])):
                 fn(self.group, self.key, self.value)
 
+
 class Modeline(Peasy.Plugin, Peasy.PluginConfigure):
     __gtype_name__ = 'Modeline'
     doc = None
     item = None
     keys = None
 
-    #The first form:
-    #       [text]{white}{vi:|vim:|ex:}[white]{options}
-    #
-    # [text]          any text or empty
-    # {white}         at least one blank character (<Space> or <Tab>)
-    # {vi:|vim:|ex:}  the string "vi:", "vim:" or "ex:"
-    # [white]         optional white space
-    # {options}       a list of option settings, separated with white space or ':',
-    #                 where each part between ':' is the argument for a ":set"
-    #                 command (can be empty)
-    #
-    # Example:
-    #    vi:noai:sw=3 ts=6 ~
-    vim_com1 = re.compile("(?:vi|vim|ex):\s*(.*[a-zA-Z0-9])$")
+    def __init__(self):
+        self.vim_com = re.compile(vim_reg)
+        self.kv_com = re.compile(kv_reg)
 
-    #      The second form (this is compatible with some versions of Vi):
-    #        [text]{white}{vi:|vim:|ex:}[white]se[t] {options}:[text]
-    #
-    # [text]          any text or empty
-    # {white}         at least one blank character (<Space> or <Tab>)
-    # {vi:|vim:|ex:}  the string "vi:", "vim:" or "ex:"
-    # [white]         optional white space
-    # se[t]           the string "set " or "se " (note the space)
-    # {options}       a list of options, separated with white space, which is the
-    #                 argument for a ":set" command
-    # :               a colon
-    # [text]          any text or empty
-    #
-    # Example:
-    #    /* vim: set ai tw=75: */ ~
-    vim_com2 = re.compile("(?:vi|vim|ex):\s?se[t\s]?\s*(.*):")
-    kv_com  = re.compile("([^:\s]*)[:\s]")
+    def apply_indent_type(self, editor, v):
+        if (self.use_setting.value == False or editor.get_indent_prefs().detect_type):
+            setattr(editor, "indent_type", v)
+        return None
 
-    # direct property assignment does not work in lambdas
+    def apply_indent_width(self, editor, v):
+        if (self.use_setting.value == False or editor.get_indent_prefs().detect_width):
+            setattr(editor, "indent_width", int(v))
+        return None
+
+    # direct attr assignment does not work in lambdas so use settattr
     procs = {
-        'expandtab'  : lambda editor     : setattr(editor, "indent_type", Geany.IndentType.SPACES),
-        'noexpandtab': lambda editor     : setattr(editor, "indent_type", Geany.IndentType.TABS),
-        'wrap'       : lambda editor     : setattr(editor, "line_wrapping", True),
-        'nowrap'     : lambda editor     : setattr(editor, "line_wrapping", False),
-        'shiftwidth' : lambda editor, val: setattr(editor, "indent_width", int(val))
+        'set'        : lambda s, editor     : None,
+        'expandtab'  : lambda s, editor     : s.apply_indent_type(editor, Geany.IndentType.SPACES),
+        'noexpandtab': lambda s, editor     : s.apply_indent_type(editor, Geany.IndentType.TABS),
+        'wrap'       : lambda s, editor     : setattr(editor, "line_wrapping", True),
+        'nowrap'     : lambda s, editor     : setattr(editor, "line_wrapping", False),
+        'shiftwidth' : lambda s, editor, val: s.apply_indent_width(editor, val)
     }
     aliases = {
+        'se'         : 'set',
+        'set'        : 'set',
         'ne'         : 'noexpandtab',
+        'noet'       : 'noexpandtab',
         'et'         : 'expandtab',
         'sw'         : 'shiftwidth',
         'tabstop'    : 'shiftwidth',
@@ -131,25 +150,19 @@ class Modeline(Peasy.Plugin, Peasy.PluginConfigure):
     }
 
     def on_match(self, line, doc):
-        m = re.findall(self.kv_com, line)
-        for x in m:
+        m = self.kv_com.split(line)
+        for x in (e for e in m if e != ''):
             kv = x.split("=")
-            try:
-                k = self.aliases[kv[0]] if kv[0] in self.aliases else kv[0]
-                if self.procs[k]:
-                    if (len(kv) == 2):
-                        v = kv[1]
-                        self.procs[k](doc.editor, v)
-                    elif (len(kv) == 1):
-                        self.procs[k](doc.editor)
-                        print (k)
-            except KeyError:
-                # unknown vim key, ignore
-                continue
-            except TypeError:
-                sys.stderr.write("invalid modeline at " + str(kv))
-                # probably invalid modeline, e.g. when a value was given to a non-value option
-                return
+            k = kv[0]
+            if (k in self.aliases):
+                k = self.aliases[k]
+            if (k in self.procs):
+                if (len(kv) == 2):
+                    self.procs[k](self, doc.editor, kv[1])
+                elif (len(kv) == 1):
+                    self.procs[k](self, doc.editor)
+            else:
+                print(_("Key %s is not supported!") % k)
 
     def parse_doc(self, obj, doc):
         sci = doc.editor.sci
@@ -157,14 +170,10 @@ class Modeline(Peasy.Plugin, Peasy.PluginConfigure):
         num_lines = sci.get_line_count()
         for x in range(0, 5):
             line = sci.get_line(x)
-            m = re.search(self.vim_com2, line)
-            if (m is None):
-                m = re.search(self.vim_com1, line)
+            m = re.search(self.vim_com, line)
             if (m is None and num_lines > 5):
                 line = sci.get_line(x + num_lines - 5)
-                m = re.search(self.vim_com2, line)
-                if (m is None):
-                    m = re.search(self.vim_com1, line)
+                m = re.search(self.vim_com, line)
             if (m is not None):
                 # Pass only in comment sections (unless the file is not highlighted at all)
                 # EXCEPT that document-open and document-reload the lexer didn't run yet
@@ -189,7 +198,6 @@ class Modeline(Peasy.Plugin, Peasy.PluginConfigure):
         self.prefs = []
         kf = ModelineKeyFile(self.geany_plugin.geany_data.app.configdir)
         self.gen_pref("use_setting", Pref("preferences", "follow-autodetect-setting", True))
-        self.gen_pref("test", Pref("preferences", "test", True))
         for pref in self.prefs:
             pref.load(kf.kf)
         return True
@@ -200,7 +208,7 @@ class Modeline(Peasy.Plugin, Peasy.PluginConfigure):
     def on_dialog_response(self, dialog, resp, widget):
         widgets = widget.get_children()
         if (resp == Gtk.ResponseType.OK or resp == Gtk.ResponseType.APPLY):
-            kf = ModelineKeyFile(self.data.app.configdir)
+            kf = ModelineKeyFile(self.geany_plugin.geany_data.app.configdir)
             kf.load()
             self.use_setting.value = widgets[0].get_active()
             self.test.value = widgets[1].get_active()
@@ -215,11 +223,41 @@ class Modeline(Peasy.Plugin, Peasy.PluginConfigure):
 
     def do_configure(self, dialog):
         c = Gtk.VBox.new(False, 6)
-        w = Gtk.CheckButton.new_with_label("Follow Geany's autodetect setting")
+        w = Gtk.CheckButton.new_with_label(_("Follow Geany's settings for detecting indentation from the file"))
+        w.set_tooltip_text(_("If enabled, indent type  and width from modeline only "+
+                             "apply if Geany is set to detect these settings from the file. "+
+                             "Other modeline settings apply always."))
         w.set_active(self.use_setting)
-        c.pack_start(w, False, False, 6)
-        w = Gtk.CheckButton.new_with_label("Test")
-        w.set_active(self.test)
         c.pack_start(w, False, False, 6)
         dialog.connect("response", self.on_dialog_response, c)
         return c
+
+
+if __name__ == '__main__':
+    print("Tests...")
+    vim_com = re.compile(vim_reg)
+    kv_com = re.compile(kv_reg)
+
+    s = vim_com.findall("vim: set sw=4 ts=3:")[0]
+    assert (s == "set sw=4 ts=3:")
+    assert (kv_com.split(s) == ["set", "sw=4", "ts=3", ""])
+
+    s = vim_com.findall("vim:sw=4 ts=3")[0]
+    assert (s == "sw=4 ts=3")
+    assert (kv_com.split(s) == ["sw=4", "ts=3"])
+
+    s = vim_com.findall("vim:sw=4:ts=3:et")[0]
+    assert (s == "sw=4:ts=3:et")
+    assert (kv_com.split(s) == ["sw=4", "ts=3", "et"])
+
+    s = vim_com.findall("vim:  et noet")[0]
+    assert(s == "et noet")
+    assert(kv_com.split(s) == ["et", "noet"])
+
+    s = vim_com.findall("vim:se et:noet:")[0]
+    assert(s == "se et:noet:")
+    assert(kv_com.split(s) == ["se", "et", "noet", ""])
+
+    s = vim_com.findall("vim:set ts=8 et:")[0]
+    assert(s == "set ts=8 et:")
+    assert(kv_com.split(s) == ["set", "ts=8", "et", ""])
