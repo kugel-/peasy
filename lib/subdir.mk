07291511bbb1d985d71a143b0428cfb69fb9c6ff
Thomas Martitz <kugel@rockbox.org>
2021-04-02 13:50:06 +0200

    libpeasy: workaround for faulty gir
    Some versions of valac generate incorrect GIR
    for callback nodes. See See https://gitlab.gnome.org/GNOME/vala/-/issues/1167
    
    Upstream is fixed but the valac versions remain in distribution archives.
    
    Fixes #14

--- lib/subdir.mk
+++ lib/subdir.mk
@@ -24,7 +24,7 @@ VALAFILES     += tags.vala
 
 # Generate secondary outputs only once, not for all .vala files
 plugin.c-VALAFLAGS-y    := --disable-version-header -H $(objdir)peasy.h
-plugin.c-VALAFLAGS-y    += --library libpeasy --gir=$(objdir)Peasy-0.1.gir
+plugin.c-VALAFLAGS-y    += --library libpeasy --gir=$(objdir)Peasy-0.0.gir
 plugin.c-VALAFLAGS-y    += --vapi=$(objdir)libpeasy-0.1.vapi
 
 vala-y                  := $(VALAFILES:.vala=.c)
@@ -44,15 +44,22 @@ keygroup.c-DEPS-y       := api/geany-1.0.vapi api/geany-scintilla-1.0.vapi
 tags.c-y                := $(VALAFILES)
 tags.c-DEPS-y           := api/geany-1.0.vapi api/geany-scintilla-1.0.vapi
 
-byproduct-y             := peasy.h Peasy-0.1.gir libpeasy-0.1.vapi
+byproduct-y             := peasy.h Peasy-0.0.gir libpeasy-0.1.vapi
 peasy.h-y               := plugin.c
-Peasy-0.1.gir-y         := plugin.c
+Peasy-0.0.gir-y         := plugin.c
 libpeasy-0.1.vapi-y     := plugin.c
 
 libpeasy.la-y := keygroup_c.c
 libpeasy.la-y += plugin_c.c
 libpeasy.la-y += $(VALAFILES:.vala=.c)
 libpeasy.la-y += peasy.h
+
+# workaround some buggy versions of valac that generate incorrect
+# GIR for callbacks. See https://gitlab.gnome.org/GNOME/vala/-/issues/1167
+sed-y         := Peasy-0.1.gir
+Peasy-0.1.gir-y := Peasy-0.0.gir
+Peasy-0.1.gir-SED_SCRIPT-y := '/<callback/{:a;N;/<\/callback\>/!ba;s/instance-parameter/parameter/g}'
+Peasy-0.1.gir-SED_SCRIPT-y += 's/version="0.0"/version="0.1"/'
 
 typelib-y     := Peasy-0.1.typelib
 typelibdata-y := Peasy-0.1.typelib
